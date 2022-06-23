475bebefaa72c6854996990ec4bcdf0df78c4359
Rick Webbernet <rick.webbernet@gmail.com>
2022-06-23 14:04:26 +0700

    Add configuration interface

--- lib/fflag/engine.rb
+++ lib/fflag/engine.rb
@@ -1,4 +1,20 @@
 module Fflag
   class Engine < ::Rails::Engine
   end
+
+  class Configuration
+    attr_accessor :cache_enable
+
+    def initialize
+      @cache_enable = false
+    end
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
 end
